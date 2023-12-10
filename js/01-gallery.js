38c02418cf537358fc51d25e3eca4d3068d2e7e1
yura-prots <yuraprots@gmail.com>
2023-12-10 16:31:30 +0200

    add galleryMarkupCreator( )

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkupCreator(galleryItems));
+
+function galleryMarkupCreator(images) {
+  return images
+    .map((image) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${image.original}">
+                    <img
+                        class="gallery__image"
+                        src="${image.preview}"
+                        data-source="large-image.jpg"
+                        alt="${image.description}"
+                    />
+                </a>
+            </li>`;
+    })
+    .join("");
+}
