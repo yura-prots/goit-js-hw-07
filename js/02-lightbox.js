f3993961fec6616c1fcbde835f455e98b4061f0c
yura-prots <yuraprots@gmail.com>
2023-12-12 00:27:25 +0200

    SimpleLightbox implemented

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkupCreator(galleryItems));
+
+function galleryMarkupCreator(images) {
+  return images
+    .map((image) => {
+      return `<li class="gallery__item">
+                    <a class="gallery__link" href="${image.original}">
+                        <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+                    </a>
+              </li>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
